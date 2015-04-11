12c5e0ddc12a1ef1346b92db68ec25c824a5f4a4
Rob Shepherd <theroshe@gmail.com>
2015-04-11 19:31:38 +0100

    Added initial bitband demo

--- freeRTOS/app/objects/misc.d
+++
@@ -1,7 +0,0 @@
-misc.o: \
- ../STM32F10x_StdPeriph_Lib_V3.5.0/Libraries/STM32F10x_StdPeriph_Driver/src/misc.c \
- ../STM32F10x_StdPeriph_Lib_V3.5.0/Libraries/STM32F10x_StdPeriph_Driver/inc/misc.h \
- ../STM32F10x_StdPeriph_Lib_V3.5.0/Libraries/CMSIS/CM3/DeviceSupport/ST/STM32F10x/stm32f10x.h \
- ../STM32F10x_StdPeriph_Lib_V3.5.0/Libraries/CMSIS/CM3/CoreSupport/core_cm3.h \
- ../STM32F10x_StdPeriph_Lib_V3.5.0/Libraries/CMSIS/CM3/DeviceSupport/ST/STM32F10x/system_stm32f10x.h \
- stm32f10x_conf.h
--- freeRTOS/app/objects/port.d
+++
@@ -1,9 +0,0 @@
-port.o: ../FreeRTOSV8.2.0/FreeRTOS/Source/portable/GCC/ARM_CM3/port.c \
- ../FreeRTOSV8.2.0/FreeRTOS/Source/include/FreeRTOS.h FreeRTOSConfig.h \
- ../FreeRTOSV8.2.0/FreeRTOS/Source/include/projdefs.h \
- ../FreeRTOSV8.2.0/FreeRTOS/Source/include/portable.h \
- ../FreeRTOSV8.2.0/FreeRTOS/Source/include/deprecated_definitions.h \
- ../FreeRTOSV8.2.0/FreeRTOS/Source/include/../../Source/portable/GCC/ARM_CM3/portmacro.h \
- ../FreeRTOSV8.2.0/FreeRTOS/Source/include/mpu_wrappers.h \
- ../FreeRTOSV8.2.0/FreeRTOS/Source/include/task.h \
- ../FreeRTOSV8.2.0/FreeRTOS/Source/include/list.h
