dc35e3fb50502a1cdc54d45346eb4ff8dc6fd988
Rob Shepherd <theroshe@gmail.com>
2015-04-11 19:51:30 +0100

    Added notes, updated openocd build script

--- openocd/build.sh
+++ openocd/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#wget http://sourceforge.net/projects/openocd/files/openocd/0.8.0/openocd-0.8.0.tar.bz2
+wget http://sourceforge.net/projects/openocd/files/openocd/0.8.0/openocd-0.8.0.tar.bz2
 tar -xvf openocd-0.8.0.tar.bz2
 cd openocd-0.8.0 
 ./configure --enable-maintainer--mode --enable-legacy-ft2232_libftdi
